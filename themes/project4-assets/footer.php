f2e29ce5efe35f19c140809e9195f917424777af
Bryan Tran <bryant.business@hotmail.com>
2019-02-22 12:59:50 -0800

    Friday before lunch (widget)

--- themes/project4-assets/footer.php
+++ themes/project4-assets/footer.php
@@ -11,8 +11,18 @@
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
 				<div class="site-info">
-
-
+			<section class = "contant_info">
+				<h3>Contact Info</h3> 
+				<span> info@inhabitent.com </span>
+				<span> 778-456-7891 </span>
+				<span class= "logos"> </span>
+			</section>
+			<section class = "business_hours">
+			<span> Monday-Friday: 9am to 5pm </span>
+			<span> Saturday: 10am to 2pm </span>
+			<span> Sunday: Closed </span>
+			</section>
+			<section class = "copyright">  
 
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
