9e261f4152506c66a0adb85ffc0d202878986574
Bryan Tran <bryant.business@hotmail.com>
2019-03-07 14:15:25 -0800

    Remove spaces and comments

--- themes/project4-assets/template-parts/content.php
+++ themes/project4-assets/template-parts/content.php
@@ -5,7 +5,6 @@
  * @package RED_Starter_Theme
  */
 ?>
-
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php if ( has_post_thumbnail() ) : ?>
@@ -17,11 +16,11 @@
 		<?php if ( 'post' === get_post_type() ) : ?>
 		<div class="entry-meta">
 			<?php red_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?>  <?php red_starter_posted_by(); ?>
-		</div><!-- .entry-meta -->
+		</div>
 		<?php endif; ?>
-	</header><!-- .entry-header -->
+	</header>
 
 	<div class="entry-content">
 		<?php the_excerpt(); ?>
-	</div><!-- .entry-content -->
-</article><!-- #post-## -->
+	</div>
+</article>
